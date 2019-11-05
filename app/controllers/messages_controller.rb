b07b7744654085c396bba7f93f88e8c259beafc5
kyouichi-inoue <corleonis81@gmail.com>
2019-11-05 16:56:13 +0900

    ルーティング設定とCarrierwave導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
+
   def index
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  
 end
